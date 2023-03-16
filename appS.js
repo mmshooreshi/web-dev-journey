8dc21ed138deb1cb874d3bcbb5d991bd83af7454
mmti <mmshooreshi@gmail.com>
2023-03-16 11:34:06 +0330

    s

--- appS.js
+++ appS.js
@@ -1,145 +1,338 @@
+// Define global variables
+const taskList = document.querySelector("#task-list");
+const taskInput = document.querySelector("#task-input");
+const taskForm = document.querySelector("#task-form");
+const taskTemplate = document.querySelector("#task-template");
+const sectionTemplate = document.querySelector("#section-template");
+const parentTemplate = document.querySelector("#parent-template");
+const tasks = {};
 
-// function to get tasks from local storage
-function getTasksFromLocalStorage() {
-	let tasks = [];
-
-	if (localStorage.getItem("tasks")) {
-		tasks = JSON.parse(localStorage.getItem("tasks"));
-	}
-
-	return tasks;
-}
-
-// function to save tasks to local storage
-function saveTasksToLocalStorage() {
-	localStorage.setItem("tasks", JSON.stringify(taskList));
-}
-
-// initialize task list from local storage
-let taskList = getTasksFromLocalStorage();
-
-// function to add a task to the task list
+// Define functions
 function addTask() {
-	// create task object from input value
-	const taskInput = document.querySelector("#task-input");
-	const task = {
-		id: Date.now(),
-		text: taskInput.value,
-		completed: false,
-		important: false
-	};
-
-	// add task to task list array
-	taskList.push(task);
-
-	// save task list to local storage
-	saveTasksToLocalStorage();
-
-	// clear input value
-	taskInput.value = "";
-
-	// render task list
-	renderTaskList();
-}
-
-// function to delete a task from the task list
-function deleteTask(taskId) {
-	// find index of task with given id in task list array
-	const taskIndex = taskList.findIndex(function(task) {
-		return task.id === taskId;
-	});
-
-	// remove task from task list array
-	taskList.splice(taskIndex, 1);
-
-	// save task list to local storage
-	saveTasksToLocalStorage();
-
-	// render task list
-	renderTaskList();
-}
-
-// function to toggle completed status of a task
-function toggleCompleted(taskId) {
-	// find task with given id in task list array
-	const task = taskList.find(function(task) {
-		return task.id === taskId;
-	});
-
-	// toggle completed status of task
-	task.completed = !task.completed;
-
-	// save task list to local storage
-	saveTasksToLocalStorage();
-
-	// render task list
-	renderTaskList();
-}
-
-// function to toggle important status of a task
-function toggleImportant(taskId) {
-	// find task with given id in task list array
-	const task = taskList.find(function(task) {
-		return task.id === taskId;
-	});
-
-	// toggle important status of task
-	task.important = !task.important;
-
-	// save task list to local storage
-	saveTasksToLocalStorage();
-
-	// render task list
-	renderTaskList();
-}
-
-// event listener for add task button
-// document.querySelector("#addTaskButton").addEventListener("click", function(event) {
-// 	event.preventDefault();
-// 	addTask();
-// });
-
-// render initial task list
-renderTaskList();
-
-
-function renderTaskList() {
-    const taskListElement = document.querySelector("#task-list ul");
-  
-    // clear current task list items
-    taskListElement.innerHTML = "";
-  
-    // render new task list items
-    taskList.forEach(function(task) {
-      const taskItemElement = document.createElement("li");
-      taskItemElement.setAttribute("data-task-id", task.id);
-  
-      const taskCheckboxElement = document.createElement("input");
-      taskCheckboxElement.type = "checkbox";
-      taskCheckboxElement.checked = task.completed;
-      taskCheckboxElement.addEventListener("click", function() {
-        toggleCompleted(task.id);
-      });
-      taskItemElement.appendChild(taskCheckboxElement);
-  
-      const taskTextElement = document.createElement("span");
-      taskTextElement.textContent = task.text;
-      if (task.important) {
-        taskTextElement.classList.add("important");
-      }
-      taskTextElement.addEventListener("click", function() {
-        toggleImportant(task.id);
-      });
-      taskItemElement.appendChild(taskTextElement);
-  
-      const taskDeleteElement = document.createElement("button");
-      taskDeleteElement.textContent = "Delete";
-      taskDeleteElement.addEventListener("click", function() {
-        deleteTask(task.id);
-      });
-      taskItemElement.appendChild(taskDeleteElement);
-  
-      taskListElement.appendChild(taskItemElement);
-    });
+  const taskName = taskInput.value.trim();
+  if (taskName === "") {
+    alert("Please enter a task name.");
+    return;
   }
-  
+
+  const task = createTask(taskName);
+  tasks[task.id] = task;
+
+  taskInput.value = "";
+  taskList.append(task.element);
+}
+
+function createTask(name) {
+  const taskElement = taskTemplate.content.cloneNode(true).firstElementChild;
+  const task = {
+    id: Date.now().toString(),
+    name: name,
+    element: taskElement,
+    children: {},
+    parent: null,
+    isCompleted: false,
+  };
+
+  const completeCheckbox = taskElement.querySelector(".task-checkbox");
+  completeCheckbox.addEventListener("change", function () {
+    task.isCompleted = completeCheckbox.checked;
+    updateTaskStatus(task);
+  });
+
+  const deleteButton = taskElement.querySelector(".task-delete-button");
+  deleteButton.addEventListener("click", function () {
+    deleteTask(task);
+  });
+
+  const addSubtaskButton = taskElement.querySelector(
+    ".task-add-subtask-button"
+  );
+  addSubtaskButton.addEventListener("click", function () {
+    const subtaskName = prompt("Please enter a name for the subtask:");
+    if (subtaskName !== null) {
+      const subtask = createTask(subtaskName);
+      task.children[subtask.id] = subtask;
+      subtask.parent = task;
+      taskElement.querySelector(".task-subtasks").append(subtask.element);
+    }
+  });
+
+  updateTaskStatus(task);
+  return task;
+}
+
+function deleteTask(task) {
+  if (task.parent !== null) {
+    delete task.parent.children[task.id];
+  }
+  delete tasks[task.id];
+  task.element.remove();
+}
+
+function updateTaskStatus(task) {
+  const completeCheckbox = task.element.querySelector(".task-checkbox");
+  completeCheckbox.checked = task.isCompleted;
+  task.element.classList.toggle("completed", task.isCompleted);
+
+  if (task.parent !== null) {
+    updateTaskStatus(task.parent);
+  }
+}
+
+function createParent(name) {
+  const parentElement =
+    parentTemplate.content.cloneNode(true).firstElementChild;
+  const parent = {
+    id: Date.now().toString(),
+    name: name,
+    element: parentElement,
+    children: {},
+    parent: null,
+    isCompleted: false,
+  };
+
+  const parentName = parentElement.querySelector(".parent-name");
+  parentName.textContent = name;
+
+  const addSectionForm = parentElement.querySelector(".add-section-form");
+  addSectionForm.addEventListener("submit", function (event) {
+    event.preventDefault();
+    const sectionName = addSectionForm
+      .querySelector(".add-section-input")
+      .value.trim();
+    if (sectionName === "") {
+      alert("Please enter a section name.");
+      return;
+    }
+
+    const section = createSection(sectionName);
+    parent.children[section.id] = section;
+    section.parent = parent;
+    parentElement.querySelector(".section-list").append(section.element);
+    addSectionForm.reset();
+  });
+
+  const deleteButton = parentElement.querySelector(".parent-delete-button");
+  deleteButton.addEventListener("click", function () {
+    deleteParent(parent);
+  });
+
+  const addSubtaskButton = taskElement.querySelector(
+    ".task-add-subtask-button"
+  );
+  addSubtaskButton.addEventListener("click", function () {
+    const subtaskName = prompt("Please enter a name for the subtask:");
+    if (subtaskName !== null) {
+      const subtask = createTask(subtaskName);
+      task.children[subtask.id] = subtask;
+      subtask.parent = task;
+      taskElement.querySelector(".task-subtasks").append(subtask.element);
+    }
+  });
+
+  updateTaskStatus(task);
+  return task;
+}
+
+function deleteTask(task) {
+  if (task.parent !== null) {
+    delete task.parent.children[task.id];
+  }
+  delete tasks[task.id];
+  task.element.remove();
+}
+
+function updateTaskStatus(task) {
+  const completeCheckbox = task.element.querySelector(".task-checkbox");
+  completeCheckbox.checked = task.isCompleted;
+  task.element.classList.toggle("completed", task.isCompleted);
+
+  if (task.parent !== null) {
+    updateTaskStatus(task.parent);
+  }
+}
+
+const sectionElement =
+  sectionTemplate.content.cloneNode(true).firstElementChild;
+const section = {
+  id: Date.now().toString(),
+  name: name,
+  element: sectionElement,
+  children: {},
+  parent: null,
+  isCompleted: false,
+};
+
+const sectionName = sectionElement.querySelector(".section-name");
+sectionName.textContent = name;
+
+const addTaskForm = sectionElement.querySelector(".add-task-form");
+addTaskForm.addEventListener("submit", function (event) {
+  event.preventDefault();
+  const taskName = addTaskForm.querySelector(".add-task-input").value.trim();
+  if (taskName === "") {
+    alert("Please enter a task name.");
+    return;
+  }
+
+  const task = createTask(taskName);
+  section.children[task.id] = task;
+  task.parent = section;
+  sectionElement.querySelector(".task-list").append(task.element);
+  addTaskForm.reset();
+});
+
+const deleteButton = sectionElement.querySelector(".section-delete-button");
+deleteButton.addEventListener("click", function () {
+  deleteSection(section);
+});
+const addSubsectionButton = sectionElement.querySelector(
+  ".section-add-subsection-button"
+);
+addSubsectionButton.addEventListener("click", function () {
+  addNewSubsection();
+});
+
+function addNewSubsection() {
+  const subsectionName = prompt("Please enter a name for the subsection:");
+  if (subsectionName !== null) {
+    const subsection = createSection(subsectionName);
+    section.children.push(subsection);
+    subsection.parent = section;
+    sectionElement.querySelector(".subsection-list").append(subsection.element);
+  }
+}
+
+function createSection(name) {
+  const id = generateUniqueId();
+  const element = document.createElement("div");
+  element.classList.add("subsection");
+  element.innerHTML = `<h3>${name}</h3><ul class="subsection-list"></ul><button class="subsection-add-subsection-button">Add Subsection</button>`;
+  const section = { id, element, parent: null, children: [] };
+  const addSubsectionButton = element.querySelector(
+    ".subsection-add-subsection-button"
+  );
+  addSubsectionButton.addEventListener("click", function () {
+    addNewSubsection(section);
+  });
+  return section;
+}
+
+function generateUniqueId() {
+  return Math.random().toString(36).substr(2, 9);
+}
+
+function deleteSection(section) {
+  if (section.parent !== null) {
+    delete section.parent.children[section.id];
+  }
+  delete tasks[section.id];
+  section.element.remove();
+}
+
+function createParent(name) {
+  const parentElement =
+    parentTemplate.content.cloneNode(true).firstElementChild;
+  const parent = {
+    id: Date.now().toString(),
+    name: name,
+    element: parentElement,
+    children: {},
+    parent: null,
+    isCompleted: false,
+  };
+
+  const parentName = parentElement.querySelector(".parent-name");
+  parentName.textContent = name;
+
+  const addSectionForm = parentElement.querySelector(".add-section-form");
+  addSectionForm.addEventListener("submit", function (event) {
+    event.preventDefault();
+    const sectionName = addSectionForm
+      .querySelector(".add-section-input")
+      .value.trim();
+    if (sectionName === "") {
+      alert("Please enter a section name.");
+      return;
+    }
+
+    const section = createSection(sectionName);
+    parent.children[section.id] = section;
+    section.parent = parent;
+    parentElement.querySelector(".section-list").append(section.element);
+    addSectionForm.reset();
+  });
+
+  const deleteButton = parentElement.querySelector(".parent-delete-button");
+  deleteButton.addEventListener("click", function () {
+    deleteParent(parent);
+  });
+
+  const addSubparentButton = parentElement.querySelector(
+    ".parent-add-subparent-button"
+  );
+  addSubparentButton.addEventListener("click", function () {
+    const subparentName = prompt("Please enter a name for the subparent:");
+    if (subparentName !== null) {
+      const subparent = createParent(subparentName);
+      parent.children[subparent.id] = subparent;
+      subparent.parent = parent;
+      parentElement
+        .querySelector(".parent-subparents")
+        .append(subparent.element);
+    }
+  });
+
+  updateParentStatus(parent);
+  return parent;
+}
+
+function deleteParent(parent) {
+  if (parent.parent !== null) {
+    delete parent.parent.children[parent.id];
+  }
+  delete tasks[parent.id];
+  parent.element.remove();
+}
+
+function updateParentStatus(parent) {
+  for (const childId in parent.children) {
+    const child = parent.children[childId];
+    if (!child.isCompleted) {
+      parent.isCompleted = false;
+      parent.element.classList.remove("completed");
+      return;
+    }
+  }
+  parent.isCompleted = true;
+  parent.element.classList.add("completed");
+
+  if (parent.parent !== null) {
+    updateParentStatus(parent.parent);
+  }
+}
+
+// Initialize the app
+const parentList = document.querySelector("#parent-list");
+
+for (const task of initialTasks) {
+  if (task.parent === null) {
+    const parent = createParent(task.name);
+    tasks[parent.id] = parent;
+    parentList.append(parent.element);
+
+    for (const childTask of task.children) {
+      const section = createSection(childTask.name);
+      parent.children[section.id] = section;
+      section.parent = parent;
+      parent.element.querySelector(".section-list").append(section.element);
+
+      for (const subchildTask of childTask.children) {
+        const task = createTask(subchildTask);
+        section.children[task.id] = task;
+        task.parent = section;
+        section.element.querySelector(".task-list").append(task.element);
+      }
+    }
+  }
+}
